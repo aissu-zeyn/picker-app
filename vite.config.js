b94d352dcc7eea7ffafe2ab3cbdac7cb19c54ee8
Aysu Zeynalova <aissu.zeyn@gmail.com>
2025-06-11 10:35:13 -0400

    chore: remove electron and update dependencies

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/picker-app/',
-  server: {
-    host: true, // This allows connections from your local network
-    port: 5173
-  }
+  base: '/picker-app/'
 })
